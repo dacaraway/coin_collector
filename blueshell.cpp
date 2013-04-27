ded0355230216240146f443caa5c062a88094caf
Daria Caraway <caraway@usc.edu>
2013-04-26 19:13:06 -0700

    commented

--- blueshell.cpp
+++ blueshell.cpp
@@ -1,5 +1,8 @@
 #include "blueshell.h"
-
+	/* Constructor for a blue shell, sets velocity to 1  
+		@param pm Pixmap pointer to show picture
+		@param nx Initial x cooridinate to set position
+		@param ny Initial y coordinate to set poition  **/
 BlueShell:: BlueShell(QPixmap *pm, int nx, int ny) : Thing(pm,nx,ny)
 {
 	*pm = pm->scaled(50, 50, Qt::KeepAspectRatio);
@@ -7,14 +10,15 @@ BlueShell:: BlueShell(QPixmap *pm, int nx, int ny) : Thing(pm,nx,ny)
 	vY = 1;
 	x = nx;
 	y = ny;
+	// bool to see if iterator should explore this object
 	goodCheck = true;
 	
 	setPos(x,y);
 } 
-
+/* Default destructor **/
 BlueShell :: ~BlueShell()
 {}
-
+/* Checks counter and sets velocities and updates position based on where item should be in square cycle **/
 void BlueShell:: move()
 {
 	if(counter <= 150)
@@ -37,30 +41,37 @@ void BlueShell:: move()
 	setPos(x,y);
 
 }
-
+/* @return 2-- tells switch in MainWindow to execute blue shell collision  **/
 int BlueShell:: executePower()
 {
 	return 2;
 }
-	
+	/* Sets the X variable of the item
+		@param nx An int passed into from MainWindow **/
 void BlueShell:: setX(int nx)
 {
 	x= nx;
 }
+/* Sets the Y variable of the item
+		@param nx An int passed into from MainWindow **/
 void BlueShell:: setY(int ny)
 {
 	y = ny;
 }
-
+/* Sets the vX variable of the item, used for velocity
+		@param nvx An int passed into from MainWindow **/
 void BlueShell:: setvX(int nvx)
 {
 	vX= nvx;
 }
+/* Sets the vY variable of the item, used for velocity
+		@param nvx An int passed into from MainWindow **/
 void BlueShell:: setvY(int nvy)
 {
 	vY = nvy;
 }
-
+/* Sets the counter to be used for square path in move()
+		@param counter1 a counter passed by MainWindow **/
 void BlueShell:: setCounter(int counter1)
 {
 	counter = counter1;
--- blueshell.h
+++ blueshell.h
@@ -2,20 +2,43 @@
 #define BLUE_SHELL_H
 #include "thing.h"
 
+/** The BlueShell class hold all the members for the blue shell item.
+@author Daria Caraway
+
+@pre Thing */
+
 class BlueShell: public Thing
 {
 	public:
+		/** Constructor for a blue shell, sets velocity to 1  
+		@param pm Pixmap pointer to show picture
+		@param nx Initial x cooridinate to set position
+		@param ny Initial y coordinate to set poition  **/
 		BlueShell(QPixmap *pm, int nx, int ny);
+		/** Default destructor **/
 		~BlueShell();
+		/** Checks counter and sets velocities and updates position based on where item should be in square cycle **/
 		void move();
+		/** @return 2-- tells switch in MainWindow to execute blue shell collision  **/
 		int executePower();
+		/** Sets the counter to be used for square path in move()
+		@param counter1 a counter passed by MainWindow **/
 		void setCounter(int counter1);
+		/** Sets the X variable of the item
+		@param nx An int passed into from MainWindow **/
 		void setX(int nx);
+		/** Sets the Y variable of the item
+		@param nx An int passed into from MainWindow **/
 		void setY(int ny);
+		/** Sets the vX variable of the item, used for velocity
+		@param nvx An int passed into from MainWindow **/
 		void setvX(int nvx);
+		/** Sets the vY variable of the item, used for velocity
+		@param nvx An int passed into from MainWindow **/
 		void setvY(int nvy);
 		
 	private:
+		/** The counter that is used in move() and set equal to the param in setCounter() **/
 		int counter;
 };
 
